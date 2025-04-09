9cb9d87ea8611fef0c57154d3860a0922bb46c04
majidali129 <itsmajidali129@gmail.com>
2025-04-09 19:47:16 +0500

    added register path

--- src/paths.ts
+++ src/paths.ts
@@ -2,3 +2,4 @@ export const homePath = () => "/";
 export const ticketsPath = () => "/tickets";
 export const ticketPath = (ticketId: string) => `/tickets/${ticketId}`;
 export const ticketUpdatePath = (ticketId: string) => `/tickets/${ticketId}/edit`;
+export const signUpPath = () => "/users/signup";
